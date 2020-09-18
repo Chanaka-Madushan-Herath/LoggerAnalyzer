859c3a99fd3d56fb0c15f10e5c5e71065911773e
Chanaka-Madushan-Herath <chanaka.herath.1998@gmail.com>
2020-09-18 10:58:00 +0530

    Email Sending part 1.3 -(Chanaka)

---
+++ src/main/java/com/make/construction/Main.java
@@ -0,0 +1,12 @@
+package com.make.construction;
+
+public class Main {
+    public static void main(String[] args) {
+
+        SendEmail sendEmail=new SendEmail();
+
+        sendEmail.sendSimpleMessage( "chanaka.herath.1998@gmail.com","chanaka.herath.1998@gmail.com","hello","testing");
+    }
+
+
+}
