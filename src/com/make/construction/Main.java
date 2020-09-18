7a789d66a2a300a58c37ec5f7d0830763ca62bb8
HansaSandamal <hansasandamal@gmail.com>
2020-09-18 07:43:31 +0530

    fileReading

--- src/com/make/construction/Main.java
+++ src/com/make/construction/Main.java
@@ -1,10 +1,39 @@
 package com.make.construction;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Scanner;
+
 public class Main {
 
-  public static void main(String[] args) throws Exception {
-    LogFileRepository logFileRepository=new LogFileRepository();
-    logFileRepository.readFile();
-  }
+    public static void main(String[] args) throws IOException {
+        String testString = null;
+        if (testString != null)
+            System.out.println(testString.equals("F"));
+
+
+    }
 
 }
+
+
+class SecondLine {
+
+    private BufferedReader bufferedReader;
+    public SecondLine(BufferedReader bufferedReader){
+
+        this.bufferedReader = bufferedReader;
+
+    }
+
+    public void print() throws IOException {
+
+        System.out.println(bufferedReader.readLine());
+
+
+    }
+}
+
